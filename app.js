4b5089d0ffaf20c5c9b240d9e0c01b689d42749b
Santosh kumar <sumitsantosh555@gmail.com>
2022-07-26 12:19:09 +0530

    Add gitignore,procofile and update port

--- app.js
+++ app.js
@@ -144,6 +144,12 @@ app.get("/about",function (req,res) {
 //   workitems.push(item);
 //   res.redirect("/work");
 // });
-app.listen(3000,function () {
-  console.log("server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port,function () {
+  console.log("server started susseccfully :");
 });
